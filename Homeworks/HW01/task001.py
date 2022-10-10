14f81f0a6267c0e251519726be4111c4d6f3eeb8
Danil Novikov <beryl@live.ru>
2022-10-10 19:40:40 +0300

    First task redone

--- Homeworks/HW01/task001.py
+++ Homeworks/HW01/task001.py
@@ -5,6 +5,10 @@
 # - 7 -> да
 # - 1 -> нет
 
+
+
+### Самый первый вариант решения задачи 
+
 # def day_off_check(day):
 #     try: 
 #         if day in range(1,6):
@@ -22,17 +26,39 @@
 
 
 
-day_off = [6,7]
-week_day = [1,2,3,4,5]
-your_day = (input('Input integer number representing the day of the week: '))
-try: 
-    if int(your_day) in day_off:
-        print('yes')
-    elif int(your_day) in week_day:
-        print('no')
-    else:
-        print('there is no such day in a week')
-except:
-    print("Try to input integer number from 1 to 7, where 1 is for Monday")
 
+### Решение задачи с использованием списков. 
+
+# day_off = [6,7]
+# week_day = [1,2,3,4,5]
+# your_day = (input('Input integer number representing the day of the week: '))
+# try: 
+#     if int(your_day) in day_off:
+#         print('yes')
+#     elif int(your_day) in week_day:
+#         print('no')
+#     else:
+#         print('there is no such day in a week')
+# except:
+#     print("Try to input integer number from 1 to 7, where 1 is for Monday")
+
+
+
+
+
+### Попытка обернуть алгоритм в функцию + добавил вывод в консоль название дня недели, попрактиковал немного словари
+
+def day_off_check(day):
+    day_off = {'6':'Saturday','7':'Sunday'}
+    week_day = {'1':'Monday','2':'Tuesday','3':'Wednesday','4':'Thursday','5':'Friday'} 
+    if day in day_off:
+         print(f"It's {day_off[day]}. Let's chill!")
+    elif day in week_day:
+        print(f"It's {week_day[day]}. Go to work!")
+    else:
+        print("Try to input integer number from 1 to 7, where 1 is for Monday")
+   
+your_day = (input('Input integer number representing the day of the week: '))
+
+day_off_check(your_day)
 
