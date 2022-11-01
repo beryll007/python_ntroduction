c9acc03b0ec51eba1a9ce3ea0a8421093979dc8c
Danil Novikov <beryl@live.ru>
2022-11-01 13:33:50 +0300

    third task is done

---
+++ Homeworks/HW05/task003.py
@@ -0,0 +1,17 @@
+# задача 3. Напишите программу, удаляющую из текста все слова, содержащие "абв". Функции FIND и COUNT юзать нельзя.
+
+def find_and_deleate_from_text(text, to_delete):
+    
+    words = text.split(' ')
+    new_words = []
+    for word in words:
+        if to_delete not in word:
+            new_words.append(word)
+    new_text = ' '.join(new_words)
+    
+    return(new_text)
+
+user_text = input('Input your text: ')
+to_delete = input('Input the part of the word you want to remove from the text: ')
+result = find_and_deleate_from_text(user_text, to_delete)
+print(result)   
